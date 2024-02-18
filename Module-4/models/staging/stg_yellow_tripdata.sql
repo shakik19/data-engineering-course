e65840f81f1362f4763508a6674dab51fb118be1
Shakik Masrafi <1ne9ine.o@gmail.com>
2024-02-18 18:14:34 +0000

    Fully completed yellow trip data model staging with some typecast and added few columns including a surrogate key trip_id and trip_type

--- Module-4/models/staging/stg_yellow_tripdata.sql
+++ Module-4/models/staging/stg_yellow_tripdata.sql
@@ -1,37 +1,43 @@
-with 
+{{ config(materialized="view") }}
 
-source as (
+with
+    tripdata as (
+        select *, row_number() over (partition by vendor_id, pickup_datetime) as rn
+        from {{ source("staging", "yellow_tripdata") }}
+        where vendor_id is not null
+    )
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(["vendor_id", "pickup_datetime"]) }} as trip_id,
+    {{ dbt.safe_cast("vendor_id", api.Column.translate_type("integer")) }} as vendor_id,
+    {{ dbt.safe_cast("rate_code", api.Column.translate_type("integer")) }} as ratecode_id,
+    {{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} as pickup_location_id,
+    {{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} as dropoff_location_id,
 
-    select * from {{ source('staging', 'yellow_tripdata') }}
+    -- timestamps
+    pickup_datetime,
+    dropoff_datetime,
 
-),
+    -- trip info
+    store_and_fwd_flag,
+    passenger_count,
+    trip_distance,
+    1 as trip_type,
 
-renamed as (
+    -- payment info
+    fare_amount,
+    extra,
+    mta_tax,
+    tip_amount,
+    tolls_amount,
+    imp_surcharge as improvement_surcharge,
+    total_amount,
+    coalesce(
+        {{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}, 0
+    ) as payment_type,
+    {{ get_payment_type_description("payment_type") }} as payment_type_description
+from tripdata
+where rn = 1
 
-    select
-        vendor_id,
-        pickup_datetime,
-        dropoff_datetime,
-        passenger_count,
-        trip_distance,
-        rate_code,
-        store_and_fwd_flag,
-        payment_type,
-        fare_amount,
-        extra,
-        mta_tax,
-        tip_amount,
-        tolls_amount,
-        imp_surcharge,
-        airport_fee,
-        total_amount,
-        pickup_location_id,
-        dropoff_location_id,
-        data_file_year,
-        data_file_month
-
-    from source
-
-)
-
-select * from renamed
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var("is_test_run", default=true) %} limit 100 {% endif %}
